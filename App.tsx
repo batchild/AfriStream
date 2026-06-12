0775d639d77c0ae056c28031f07016d2f1149931
batchild <batchayachivan1@gmail.com>
2026-06-12 05:22:59 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ import { parseM3U, GroupedChannels, Channel } from './services/m3uParser';
 import { StreamPlayer } from './components/StreamPlayer';
 import { ChannelCard } from './components/ChannelCard';
 
-const M3U_PLAYLIST_URL = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"; 
+const M3U_PLAYLIST_URL = "https://iptv-org.github.io/iptv/index.m3u"; 
 
 export default function App() {
   const [allChannels, setAllChannels] = useState<Channel[]>([]);
