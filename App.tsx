ad9ccf6997f4d75682edcdb627c1c234d19a3d6c
batchild <batchayachivan1@gmail.com>
2026-06-12 04:43:29 +0100

    Update M3U playlist URL to sports category

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ import { parseM3U, GroupedChannels, Channel } from './services/m3uParser';
 import { StreamPlayer } from './components/StreamPlayer';
 import { ChannelCard } from './components/ChannelCard';
 
-const M3U_PLAYLIST_URL = "https://catalyze-visible-sixties.ngrok-free.dev/output/m3u"; 
+const M3U_PLAYLIST_URL = "https://iptv-org.github.io/iptv/categories/sports.m3u"; 
 
 export default function App() {
   const [allChannels, setAllChannels] = useState<Channel[]>([]);
